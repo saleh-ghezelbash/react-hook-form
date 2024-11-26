3fa0abd4eb993a5d80d56d1e16b000bc071602b4
saleh <s.ghezelbash10@gmail.com>
2024-11-26 23:43:54 +0330

    First commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { TodoContext } from '../contexts/TodoContext';
+import { TodoContextType } from '../contexts/TodoContextType';
+import TodoListItem from './TodoListItem';
+// import Navbar from './Navbar';
+
+const TodoList = () => {
+    const { todos } = useContext<TodoContextType>(TodoContext);
+
+    return (
+        <>
+            {/* <Navbar /> */}
+            <table className="uk-table">
+                <caption>Lista de tarefas</caption>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Tarefa</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        todos?.map(
+                            todo => (<TodoListItem key={todo.id} todo={todo}></TodoListItem>)
+                        )
+                    }
+                </tbody>
+
+            </table>
+        </>
+    )
+}
+
+export default TodoList;
